313a8ca3bd41667dd009d22bc09cb3c5d0c16775
Pavel-Kolenda <115988108+Pavel-Kolenda@users.noreply.github.com>
2024-03-09 16:48:41 +0300

    Add Lab21

---
+++ Lab21/Lab21/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab21.Controllers;
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Lab21/Lab21/Controllers/Task4Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab21.Controllers
+{
+    public class Task4Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
